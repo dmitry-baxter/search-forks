8f1dc5fdbf9e1a6957dadc64d25295dbc929f6d8
Dmitry Baxter <eddmonsmith@gmail.com>
2020-05-18 08:08:11 +0600

    commit

---
+++ client/build/precache-manifest.ef56960bc4f5be0bca95d483dc977920.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bde41d16e246e1cf8b9e6b3e9978b4cf",
+    "url": "/index.html"
+  },
+  {
+    "revision": "dfa2ea6501f43ef6b4bc",
+    "url": "/static/js/2.b33681fe.chunk.js"
+  },
+  {
+    "revision": "dcac967cc7ff69e42b4a8c6865540ce9",
+    "url": "/static/js/2.b33681fe.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0b77816d368f800c9120",
+    "url": "/static/js/main.70b3d0ef.chunk.js"
+  },
+  {
+    "revision": "6f0234b4b87d585d06e8",
+    "url": "/static/js/runtime-main.cfae3202.js"
+  }
+]);
